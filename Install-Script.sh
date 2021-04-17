5aae5c6e1e85a60a33f6877f1c035211044d00a3
PepperoniPingu <77292980+PepperoniPingu@users.noreply.github.com>
2021-04-17 20:19:25 +0200

    Added auto-yes

--- Install-Script.sh
+++ Install-Script.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sudo apt-get install boinc-client 
+sudo apt-get install boinc-client -y
 boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc
