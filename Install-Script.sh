e72a452d63697d364d34d204e041b0d1bee1641e
PepperoniPingu <77292980+PepperoniPingu@users.noreply.github.com>
2021-04-20 17:32:50 +0200

    Automatic starting

--- Install-Script.sh
+++ Install-Script.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-sudo apt-get install boinc-client -y
-boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc
+apt-get install boinc-client -y
+apt-get install screen -y
+echo "#! /bin/bash" >> /etc/rc.local
+echo "screen boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+screen boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc
