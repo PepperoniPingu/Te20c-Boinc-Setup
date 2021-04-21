47fff9c22d583902a17f8d85714fa6b636ab0533
PepperoniPingu <77292980+PepperoniPingu@users.noreply.github.com>
2021-04-21 09:25:37 +0200

    Update Install-Script.sh

--- Install-Script.sh
+++ Install-Script.sh
@@ -3,11 +3,11 @@
 apt-get install boinc-client -y
 apt-get install screen -y
 
-echo "~/Start-WCG.sh" >> ./.profile
-
 touch ./Start-WCG.sh
 echo "#! /bin/bash" >> ./Start-WCG.sh
 echo "sudo screen -S WCG boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ./Start-WCG.sh
 chmod =rwx ./Start-WCG.sh
+
+echo "~/Start-WCG.sh" >> ./.profile
 
 ./Start-WCG.sh
