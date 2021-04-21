14f3e828941af6153d268a1948543fed32a71338
PepperoniPingu <77292980+PepperoniPingu@users.noreply.github.com>
2021-04-21 08:42:54 +0200

    Update Install-Script.sh

--- Install-Script.sh
+++ Install-Script.sh
@@ -3,14 +3,14 @@
 apt-get install boinc-client -y
 apt-get install screen -y
 
+touch ~/Start-WCG.sh
+echo "#! /bin/bash" >> ~/Start-WCG.sh
+echo "sudo screen -S WCG boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ~/Start-WCG.sh
+chmod =rwx ~/Start-WCG.sh
+
 touch /etc/rc.local
 echo "#! /bin/bash" >> /etc/rc.local
-echo "screen -S WCG boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> /etc/rc.local
+echo "~/Start-WCG.sh" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
 
-touch ./Start-WCG.sh
-echo "#! /bin/bash" >> ./Start-WCG.sh
-echo "sudo screen -S WCG boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ./Start-WCG.sh
-chmod =rwx ./Start-WCG.sh
-
-sudo screen -S WCG boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc
+~/Start-WCG.sh
