7edaa48bd2f3804f0e15fb42259d8a545044f222
PepperoniPingu <77292980+PepperoniPingu@users.noreply.github.com>
2021-04-22 13:30:26 +0200

    Update Install-Script.sh

--- Install-Script.sh
+++ Install-Script.sh
@@ -5,7 +5,7 @@ apt-get install screen -y
 
 touch ./Start-WCG.sh
 echo "#! /bin/bash" >> ./Start-WCG.sh
-echo "screen -S WCG sudo boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ./Start-WCG.sh
+echo "screen -S WCG boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ./Start-WCG.sh
 chmod =rwx ./Start-WCG.sh
 
 echo "~/Start-WCG.sh" >> ./.profile
