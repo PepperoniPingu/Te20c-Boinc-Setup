e98129559e9da8d2de31d92ec16e4197ea1d7f7e
PepperoniPingu <77292980+PepperoniPingu@users.noreply.github.com>
2021-04-25 15:35:10 +0200

    Update Install-Script.sh

--- Install-Script.sh
+++ Install-Script.sh
@@ -8,6 +8,6 @@ echo "#! /bin/bash" >> ./Start-WCG.sh
 echo "screen -S WCG sudo boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ./Start-WCG.sh
 chmod 777 ./Start-WCG.sh
 
-echo "~/Start-WCG.sh" >> ./.profile
+echo "./Start-WCG.sh" >> ./.profile
 
 ./Start-WCG.sh
