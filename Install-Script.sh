ef618aa2c64203d01052a7e369975cee76c6af97
PepperoniPingu <77292980+PepperoniPingu@users.noreply.github.com>
2021-04-25 18:09:41 +0200

    Update Install-Script.sh

--- Install-Script.sh
+++ Install-Script.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
 apt-get install boinc-client -y
 apt-get install screen -y
 
 touch ./Start-WCG.sh
 echo "#! /bin/bash" >> ./Start-WCG.sh
-echo "screen -S WCG sudo boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ./Start-WCG.sh
+echo "setpriv --reuid=1000 --regid=1000 --init-groups screen -S WCG boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ./Start-WCG.sh
 chmod 777 ./Start-WCG.sh
 
 echo "./Start-WCG.sh" >> ./.profile
