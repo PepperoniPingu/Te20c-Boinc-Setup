94abf54642ee98222e00728b980aa161738a596e
PepperoniPingu <77292980+PepperoniPingu@users.noreply.github.com>
2021-04-26 08:50:33 +0200

    Update Install-Script.sh

--- Install-Script.sh
+++ Install-Script.sh
@@ -10,9 +10,9 @@ apt-get install screen -y
 
 touch ./Start-WCG.sh
 echo "#! /bin/bash" >> ./Start-WCG.sh
-echo "setpriv --reuid=1000 --regid=1000 --init-groups screen -S WCG boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ./Start-WCG.sh
+echo "screen -S WCG boinc --attach_project http://www.worldcommunitygrid.org/  1142594_4f34e73e8c27f9d6d16b3f76a3e27031 --no_gui_rpc" >> ./Start-WCG.sh
 chmod 777 ./Start-WCG.sh
 
 echo "./Start-WCG.sh" >> ./.profile
 
-./Start-WCG.sh
+setpriv --reuid=1000 --regid=1000 --init-groups bash ./Start-WCG.sh
